330bb789208eae8724639bac0b302a9017b66fe5
Jorge Peña <eggotron@gmail.com>
2025-10-13 17:11:12 -0600

    Aplicación de cambios

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 echo file_get_contents("views/layouts/header.html");
-echo file_get_contents("views/layouts/utilities/_guiar_nav.html");
 echo file_get_contents("views/index.html");
 echo file_get_contents("views/layouts/utilities/_model_modals.html");
 echo file_get_contents("views/layouts/footer.html");
--- privacy.php
+++ privacy.php
@@ -1,6 +1,5 @@
 <?php
 echo file_get_contents("views/layouts/header.html");
-echo file_get_contents("views/layouts/utilities/_guiar_nav.html");
 echo file_get_contents("views/privacy.html");
 echo file_get_contents("views/layouts/footer.html");
 ?>
